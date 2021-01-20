58053f73ae81ad5111c0cd30f21fcc711a0fac88
Dave Platt <dave.platt@bris.ac.uk>
2021-01-20 18:23:57 +0000

    Added L1_half_arb directory

---
+++ L1_half_arb/L_half.c
@@ -0,0 +1,100 @@
+#include "inttypes.h"
+#include "acb_dirichlet.h"
+#define MAX_Q (10000)
+
+uint64_t gcd (uint64_t a, uint64_t b)
+/* Euclid algorithm gcd */
+{
+  uint64_t c;
+  while(a!=0)
+    {
+      c=a;
+      a=b%a;
+      b=c;
+    };
+  return(b);
+};
+
+int co_prime(uint64_t a, uint64_t b)
+{
+  return(gcd(a,b)==1);
+};
+
+
+int main()
+{
+  int64_t prec=200;
+  arb_t sqrt_q;
+  dirichlet_group_t G;
+  acb_dirichlet_hurwitz_precomp_t pre;
+  acb_t tmp;
+  acb_init(tmp);
+  arb_set_d(acb_realref(tmp),0.5);
+  acb_dirichlet_hurwitz_precomp_init_num(pre,tmp,0,10000.0,prec);
+  acb_clear(tmp);
+  arb_init(sqrt_q);
+
+  
+  acb_t w[MAX_Q],v[MAX_Q];
+  
+  for(uint64_t i=0;i<MAX_Q;i++)
+    {
+      acb_init(w[i]);
+      acb_init(v[i]);
+    }
+
+  /*
+  acb_set_d(w[0],0.5);
+  acb_set_d(w[1],0.25);
+  acb_dirichlet_hurwitz(w[2],w[0],w[1],prec);
+  printf("With hurwitz directly ");acb_printd(w[2],20);
+
+  acb_dirichlet_hurwitz_precomp_eval(w[2],pre,1,4,prec);
+  printf("\nWith pre-hurwitz ");acb_printd(w[2],20);
+  return 0;
+  */
+
+  dirichlet_char_t ch;
+
+  
+  for(uint64_t q=3;q<=MAX_Q;q++)
+    {
+      if((q%4)==2) continue;
+      arb_sqrt_ui(sqrt_q,q,prec);
+      dirichlet_group_init(G,q);
+
+
+  
+      for(uint64_t i=1;i<q;i++)
+	if(co_prime(i,q))
+	  acb_dirichlet_hurwitz_precomp_eval(v[i],pre,i,q,prec);
+
+      acb_dirichlet_dft((acb_ptr) w,(acb_srcptr) v,G,prec);
+
+      dirichlet_char_init(ch,G);
+      dirichlet_char_first_primitive(ch,G);
+      while(1==1)
+	{
+	  uint64_t i=dirichlet_char_exp(G,ch);
+	  acb_div_arb(w[i],w[i],sqrt_q,prec);
+	  printf("%lu %lu: ",q,i);
+	  acb_printd(w[i],20);
+	  printf("\n");
+	  if(dirichlet_char_next_primitive(ch,G)<0)
+	    break;
+	}
+      dirichlet_char_clear(ch); 
+      dirichlet_group_clear(G);
+    }
+
+  for(uint64_t i=0;i<MAX_Q;i++)
+    {
+      acb_clear(w[i]);
+      acb_clear(v[i]);
+    }
+
+  arb_clear(sqrt_q);
+  acb_dirichlet_hurwitz_precomp_clear(pre);  
+  
+  return 0;
+}
